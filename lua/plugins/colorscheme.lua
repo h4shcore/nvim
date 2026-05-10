b4a3978a9d9519b369a7265d014a63f6df48d0a2
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-05-10 17:21:28 +0530

    added base46 theme for dms integration

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,37 @@
 return {
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("gruvbox")
+
+  --     -- 🔥 FINAL override (after everything)
+  --     vim.schedule(function()
+  --       local groups = {
+  --         "SignColumn",
+  --         -- "LineNr",
+  --         -- "CursorLineNr",
+  --         "FoldColumn",
+
+  --         "DiagnosticSignWarn",
+  --         "DiagnosticSignError",
+  --         "DiagnosticSignInfo",
+  --         "DiagnosticSignHint",
+  --       }
+
+  --       for _, group in ipairs(groups) do
+  --         vim.api.nvim_set_hl(0, group, { bg = "none" })
+  --       end
+  --     end)
+  --   end,
+  -- },
+
   {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("gruvbox")
-
-      -- 🔥 FINAL override (after everything)
-      vim.schedule(function()
-        local groups = {
-          "SignColumn",
-          -- "LineNr",
-          -- "CursorLineNr",
-          "FoldColumn",
-
-          "DiagnosticSignWarn",
-          "DiagnosticSignError",
-          "DiagnosticSignInfo",
-          "DiagnosticSignHint",
-        }
-
-        for _, group in ipairs(groups) do
-          vim.api.nvim_set_hl(0, group, { bg = "none" })
-        end
-      end)
+    "AvengeMedia/base46",
+    lazy = true,
+    opts = {},
+    config = function ()
+      vim.cmd.colorscheme("dms")
     end,
   },
 }
