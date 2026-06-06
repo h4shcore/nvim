f95d280566953e175c5cfecd86b1e0f3e95bd2f5
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-06-06 22:14:32 +0530

    fixed lualine theme

--- lua/matugen.lua
+++ lua/matugen.lua
@@ -2,21 +2,21 @@
 
 function M.setup()
   require('base16-colorscheme').setup({
-    base00 = '#1d100d',
-    base01 = '#2a1c18',
-    base02 = '#362622',
-    base03 = '#a98a81',
-    base04 = '#e2bfb6',
-    base05 = '#f7ddd6',
-    base06 = '#f7ddd6',
-    base07 = '#f7ddd6',
+    base00 = '#121315',
+    base01 = '#1e2021',
+    base02 = '#292a2c',
+    base03 = '#8d9197',
+    base04 = '#c3c7cd',
+    base05 = '#e3e2e4',
+    base06 = '#e3e2e4',
+    base07 = '#e3e2e4',
     base08 = '#ffb4ab',
-    base09 = '#ebc24d',
-    base0A = '#ffb5a0',
-    base0B = '#ffb5a0',
-    base0C = '#eac24d',
-    base0D = '#ffb5a0',
-    base0E = '#ffb5a0',
+    base09 = '#debcdf',
+    base0A = '#bec7d4',
+    base0B = '#aec9e6',
+    base0C = '#debcdf',
+    base0D = '#aec9e6',
+    base0E = '#bec7d4',
     base0F = '#93000a',
   })
 
@@ -24,18 +24,18 @@ function M.setup()
     vim.api.nvim_set_hl(0, group, opts)
   end
 
-  hi('TelescopeNormal',         { fg = '#f7ddd6',          bg = '#1d100d' })
-  hi('TelescopeBorder',         { fg = '#a98a81',             bg = '#1d100d' })
-  hi('TelescopePromptNormal',   { fg = '#f7ddd6',          bg = '#1d100d' })
-  hi('TelescopePromptBorder',   { fg = '#a98a81',             bg = '#1d100d' })
-  hi('TelescopePromptPrefix',   { fg = '#ffb5a0',             bg = '#1d100d' })
-  hi('TelescopePromptCounter',  { fg = '#e2bfb6',  bg = '#1d100d' })
-  hi('TelescopePromptTitle',    { fg = '#1d100d',             bg = '#ffb5a0' })
-  hi('TelescopePreviewTitle',   { fg = '#1d100d',             bg = '#ffb5a0' })
-  hi('TelescopeResultsTitle',   { fg = '#1d100d',             bg = '#ebc24d' })
-  hi('TelescopeSelection',      { fg = '#f7ddd6',          bg = '#362622' })
-  hi('TelescopeSelectionCaret', { fg = '#ffb5a0',             bg = '#362622' })
-  hi('TelescopeMatching',       { fg = '#ffb5a0',             bold = true })
+  hi('TelescopeNormal',         { fg = '#e3e2e4',          bg = '#121315' })
+  hi('TelescopeBorder',         { fg = '#8d9197',             bg = '#121315' })
+  hi('TelescopePromptNormal',   { fg = '#e3e2e4',          bg = '#121315' })
+  hi('TelescopePromptBorder',   { fg = '#8d9197',             bg = '#121315' })
+  hi('TelescopePromptPrefix',   { fg = '#aec9e6',             bg = '#121315' })
+  hi('TelescopePromptCounter',  { fg = '#c3c7cd',  bg = '#121315' })
+  hi('TelescopePromptTitle',    { fg = '#121315',             bg = '#aec9e6' })
+  hi('TelescopePreviewTitle',   { fg = '#121315',             bg = '#bec7d4' })
+  hi('TelescopeResultsTitle',   { fg = '#121315',             bg = '#debcdf' })
+  hi('TelescopeSelection',      { fg = '#e3e2e4',          bg = '#292a2c' })
+  hi('TelescopeSelectionCaret', { fg = '#aec9e6',             bg = '#292a2c' })
+  hi('TelescopeMatching',       { fg = '#aec9e6',             bold = true })
 end
 
  -- Register a signal handler for SIGUSR1 (matugen updates)
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
   require('lualine').setup {
     options = {
       icons_enabled = false,
-      theme = 'dms',
+      theme = 'auto',
       -- component_separators = { left = '', right = ''},
       -- section_separators = { left = '', right = ''},
       component_separators = { left = '::', right = '::'},
