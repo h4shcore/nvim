2b4d00e0426652cd56e6a2533724fd19c2181ce0
h4shcore <97403914+h4shcore@users.noreply.github.com>
2026-06-07 15:14:16 +0530

    added support for haskell

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,138 +1,146 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "stevearc/conform.nvim",
-    },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"stevearc/conform.nvim",
+		},
 
-    config = function()
-      -- Mason
-      require("mason").setup()
+		config = function()
+			-- Mason
+			require("mason").setup()
 
-      -- LSP
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "rust_analyzer",
-          "clangd",
-          "nil_ls",
-        },
-      })
+			-- LSP
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"rust_analyzer",
+					"clangd",
+					"nil_ls",
+				},
+			})
 
-      -- Capabilities
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+			-- Capabilities
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-      -- Rust
-      vim.lsp.config("rust_analyzer", {
-        capabilities = capabilities,
-        settings = {
-          ["rust-analyzer"] = {
-            cargo = { allFeatures = true },
-            checkOnSave = true,
-            check = { command = "clippy" },
-          },
-        },
-      })
-      vim.lsp.enable("rust_analyzer")
+			-- Rust
+			vim.lsp.config("rust_analyzer", {
+				capabilities = capabilities,
+				settings = {
+					["rust-analyzer"] = {
+						cargo = { allFeatures = true },
+						checkOnSave = true,
+						check = { command = "clippy" },
+					},
+				},
+			})
+			vim.lsp.enable("rust_analyzer")
 
-      -- C/C++
-      vim.lsp.config("clangd", {
-        capabilities = capabilities,
-        cmd = { "clangd", "--background-index" },
-      })
-      vim.lsp.enable("clangd")
+			-- C/C++
+			vim.lsp.config("clangd", {
+				capabilities = capabilities,
+				cmd = { "clangd", "--background-index" },
+			})
+			vim.lsp.enable("clangd")
 
-      -- Nix 
-      vim.lsp.config("nil_ls", {
-        capabilities = capabilities,
-        settings = {
-          ["nil"] = {
-            formatting = {
-              command = { "alejandra" },
-            },
-          },
-        },
-      })
-      vim.lsp.enable("nil_ls")
+			-- Nix
+			vim.lsp.config("nil_ls", {
+				capabilities = capabilities,
+				settings = {
+					["nil"] = {
+						formatting = {
+							command = { "alejandra" },
+						},
+					},
+				},
+			})
+			vim.lsp.enable("nil_ls")
 
-      -- Completion
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
+			-- Haskell
+			vim.lsp.config("hls", {
+				capabilities = capabilities,
+			})
+			vim.lsp.enable("hls")
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
+			-- Completion
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
 
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        },
-      })
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
 
-      -- Formatting
-      require("conform").setup({
-        formatters_by_ft = {
-          nix = { "alejandra" },
-          rust = { "rustfmt" },
-          c = { "clang-format" },
-          cpp = { "clang-format" },
-        },
-      })
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				},
+			})
 
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = { "*.nix", "*.rs", "*.c", "*.cpp" },
-        callback = function()
-          require("conform").format({ lsp_fallback = true })
-        end,
-      })
+			-- Formatting
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					nix = { "alejandra" },
+					rust = { "rustfmt" },
+					c = { "clang-format" },
+					cpp = { "clang-format" },
+					haskell = { "ormolu" },
+				},
+			})
 
-      -- Keymaps
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local buf = args.buf
-          local map = vim.keymap.set
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = { "*.lua", "*.nix", "*.rs", "*.c", "*.cpp", "*.hs" },
+				callback = function()
+					require("conform").format({ lsp_fallback = true })
+				end,
+			})
 
-          map("n", "gd", vim.lsp.buf.definition, { buffer = buf })
-          map("n", "gk", vim.lsp.buf.hover, { buffer = buf })
-          map("n", "<leader>rn", vim.lsp.buf.rename, { buffer = buf })
-          map("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = buf })
-        end,
-      })
-    end,
-  },
+			-- Keymaps
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local buf = args.buf
+					local map = vim.keymap.set
+
+					map("n", "gd", vim.lsp.buf.definition, { buffer = buf })
+					map("n", "gk", vim.lsp.buf.hover, { buffer = buf })
+					map("n", "<leader>rn", vim.lsp.buf.rename, { buffer = buf })
+					map("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = buf })
+				end,
+			})
+		end,
+	},
 }
